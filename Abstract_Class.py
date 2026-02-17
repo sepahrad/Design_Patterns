7bfa43592ccc825a745e81cc791c3930ac287f81
Sepahrad Salour <sepahrad.salour@gmail.com>
2026-02-17 13:42:17 +0000

    Adding some comments.

--- Abstract_Class.py
+++ Abstract_Class.py
@@ -1,6 +1,8 @@
 # Prevent user from creating an object of the abstract class and to ensure that the child class has implemented all the abstract methods of the parent class, we can use the abc module in Python. 
 # The abc module provides a way to define abstract base classes (ABCs) and abstract methods.
 
+# We need at least one abstract method in the abstract class. 
+# An abstract method is a method that is declared, but contains no implementation.
 from abc import ABC, abstractmethod
 
 class Vehicle(ABC):
@@ -8,6 +10,7 @@ class Vehicle(ABC):
     def go(self):
         pass
 
+# The Car class inherits from the Vehicle class and implements the go() method, which is required by the abstract base class.
 class Car(Vehicle):
     def go(self):
         print("The car is moving")
